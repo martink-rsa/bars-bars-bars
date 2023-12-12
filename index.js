3888c9ac542c29e606f428e2ece3b40fc8dae67e
Martin Kruger <52622303+martink-rsa@users.noreply.github.com>
2023-12-12 20:31:58 +0200

    none: added track

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
+let isTrackPlaying = false;
+
 const bars1 = new Howl({
   src: ['./bars.mp3'],
 });
 const bars3 = new Howl({
   src: ['./bars-3.mp3'],
+});
+const track = new Howl({
+  src: ['./track.mp3'],
+  loop: true,
+  volume: 0.6,
 });
 
 const barsButton = document.getElementById('bars-button');
@@ -14,3 +21,18 @@ const barsBarsBarsButton = document.getElementById('bars-bars-bars-button');
 barsBarsBarsButton.addEventListener('click', () => {
   bars3.play();
 });
+
+const trackButton = document.getElementById('track-button');
+trackButton.addEventListener('click', () => {
+  toggleTrack();
+});
+
+function toggleTrack() {
+  if (!isTrackPlaying) {
+    track.play();
+    isTrackPlaying = true;
+  } else {
+    track.stop();
+    isTrackPlaying = false;
+  }
+}
